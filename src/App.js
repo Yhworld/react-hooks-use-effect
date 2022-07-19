899b69bfc6877cadc2381f0384bdd740a84a30ef
Yahya <yahyahussein670@gmail.com>
2022-07-19 08:13:42 +0300

    commit tree 554c584f95c770fedeee5ac87506da62d1c090f9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    setTimeout(() => setCount(0), 5000);
+  }, [count]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Click Me {count}
+      </button>
+      <DogPics handleClick = {handleClick}/>
+    </div>
+  );
 }
 
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 
+
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
+function DogPics({handleClick}) {
   const [images, setImages] = useState([]);
+
 
   useEffect(() => {
     console.log("useEffect");
@@ -13,14 +15,14 @@ function DogPics() {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  }, [handleClick]);
 
   console.log("render");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt = "rrrrrrrr" />
       ))}
     </div>
   );
